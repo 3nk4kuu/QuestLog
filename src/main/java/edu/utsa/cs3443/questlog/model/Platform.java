21dfac618930b0b9ac67f350cafe7770bae0747c
3nk4kuu <keziah.muyna@my.utsa.edu>
2025-12-03 13:54:07 -0600

    updated how enum names are displayed

--- src/main/java/edu/utsa/cs3443/questlog/model/Platform.java
+++ src/main/java/edu/utsa/cs3443/questlog/model/Platform.java
@@ -1,6 +1,31 @@
 package edu.utsa.cs3443.questlog.model;
 
 public enum Platform {
-    PC, PLAYSTATION, XBOX, SWITCH, OTHER
+    PC("PC", "PC"),
+    PLAYSTATION("PlayStation", "PS"),
+    XBOX("Xbox", "XB"),
+    SWITCH("Nintendo Switch", "NS"),
+    OTHER("Other", "Other");
+
+    private final String displayName;
+    private final String shortName;
+
+    Platform(String displayName, String shortName) {
+        this.displayName = displayName;
+        this.shortName = shortName;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public String getShortCode() {
+        return shortName;
+    }
+
+    @Override
+    public String toString() {
+        return displayName;
+    }
 }
 
--- src/main/java/edu/utsa/cs3443/questlog/model/Status.java
+++ src/main/java/edu/utsa/cs3443/questlog/model/Status.java
@@ -1,6 +1,17 @@
 package edu.utsa.cs3443.questlog.model;
 
 public enum Status {
-    BACKLOG, PLAYING, COMPLETED, ON_HOLD, DROPPED
+    BACKLOG("Backlog"),
+    PLAYING("Playing"),
+    COMPLETED("Completed"),
+    ON_HOLD("On Hold"),
+    DROPPED("Dropped");
+
+    private final String display;
+
+    Status(String display) { this.display = display; }
+
+    @Override
+    public String toString() { return display; }
 }
 
