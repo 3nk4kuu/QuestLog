fb5ece9cb94266944fefd2308a2844ae9854b29b
3nk4kuu <keziah.muyna@my.utsa.edu>
2025-12-09 01:01:22 -0600

    resized game art image size and adjusted page spacing & margins

--- src/main/java/edu/utsa/cs3443/questlog/model/Platform.java
+++ src/main/java/edu/utsa/cs3443/questlog/model/Platform.java
@@ -2,7 +2,7 @@ package edu.utsa.cs3443.questlog.model;
 
 public enum Platform {
     PC("PC", "PC"),
-    PLAYSTATION("Play Station", "PS"),
+    PLAYSTATION("PlayStation", "PS"),
     XBOX("Xbox", "XB"),
     SWITCH("Nintendo Switch", "NS"),
     OTHER("Other", "Other");
